50b4059bf2e068fb05823dadc30e59fa47a35db8
Na7ha <npinzonm1@ucentral.edu.co>
2024-10-16 15:23:31 -0500

    FrontendV1

--- seguros-confianza/server/main.server.mjs
+++ seguros-confianza/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-QEHZUYZG.mjs";import"./chunk-NI2CGD3B.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-6Z445AKJ.mjs";import"./chunk-FUME3BZN.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
