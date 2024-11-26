5c0c6c6e27f21911251ad7b5c70b6324f2ad0941
Na7ha <npinzonm1@ucentral.edu.co>
2024-11-26 00:01:17 -0500

    Restaurando carpeta Fronted/Seguros_Confianza

--- Fronted/Seguros_Confianza/Backend/segurosConfianza/projects/serializers.py
+++
@@ -1,8 +0,0 @@
-# projects/serializers.py
-from rest_framework import serializers
-from .models import Facecolda
-
-class FacecoldaSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Facecolda
-        fields = '__all__'  
