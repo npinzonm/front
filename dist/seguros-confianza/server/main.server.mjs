63af89762a81339a996dc4403fd5b6078e555d7a
Na7ha <npinzonm1@ucentral.edu.co>
2024-11-25 20:01:26 -0500

    Agregando solo la carpeta dist

--- dist/seguros-confianza/server/main.server.mjs
+++ dist/seguros-confianza/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-I4SXWYXS.mjs";import"./chunk-ZQUKP2LY.mjs";import"./chunk-YJRZK3GP.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-CRAA4MWG.mjs";import"./chunk-ZQUKP2LY.mjs";import"./chunk-YJRZK3GP.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
